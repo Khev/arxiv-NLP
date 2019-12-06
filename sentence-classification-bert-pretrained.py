dcbcf11da119e169e641ce33dcde2ab6a0eb1b05
khev <kevin.p.okeeffe@gmail.com>
2019-12-06 15:32:15 -0500

    debugging sentence classification

--- sentence-classification-bert-pretrained.py
+++ sentence-classification-bert-pretrained.py
@@ -142,26 +142,51 @@ def flat_accuracy(preds, labels):
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
 
 
-def evaluate(preds,labels):
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0) 
 
-    #Compute stats
+
+def evaluate(logits,labels):
+    """
+    Calculates for a batch:
+    
+    1. Logliklihood
+    2. The number of times the y_true lies in the top1, top3, top5 predictions
+    
+    These are NOT averaged over; since this operates on batches, the 
+    averaging is done later.
+    
+    Defn: prob = softmax(logits)
+          perp = 2*( -1/N * sum_i log_2(p_i)  )   --> NOTE b
+    
+    """
+
+    #compute stats
     logliklihood = 0
     top1, top3, top5 = 0,0,0
-    indicies_top5 = np.flip(np.argsort(preds,axis=1),axis=1)[:,:5]
-    for i,row in enumerate(indicies_top5):
-        label = labels[i]
-        index_max = row[0]
-        logit = logits[i][index_max]
-        prob = 1.0 / (1.0 + np.exp(-logit))
-        logliklihood += np.log(prob)
-        if row[0] == label:
+
+    #main sum
+    for logit, label in zip(logits,labels):
+
+        #find perplexity
+        label = int(label)
+        probs = softmax(logit)
+        prob_true = probs[label]                   #the logit of y_true
+        logliklihood += np.log2(prob_true)         #note base2
+
+        #stuff for top_n
+        sorted_indicies = np.argsort(probs)[::-1]
+        top5_logits = sorted_indicies[:5]
+        if top5_logits[0] == label:
             top1 += 1
-        if label in row[:3]:
+        if label in top5_logits[:3]:
             top3 += 1
-        if label in row[:5]:
+        if label in top5_logits:
             top5 += 1
-    acc1, acc3, acc5 = top1 / len(labels), top3 / len(labels), top5 / len(labels)
-    return acc1, acc3, acc5, logliklihood
+
+    return top1, top3, top5, logliklihood
 
 
 
@@ -177,7 +202,7 @@ if __name__ == '__main__':
     parser.add_argument('data_type', type=str, help='options = [title,abstract]')
     parser.add_argument('--gpu', type=bool, default=True,  help='use GPU or not')
     parser.add_argument('--batch_size',type=int,default=4, help='number of samples per batch to GPU')
-    parser.add_argument('--epochs', type=int, default = 2, help='number of epochs')
+    parser.add_argument('--epochs', type=int, default = 5, help='number of epochs')
 
     args = parser.parse_args()
     gpu = args.gpu
@@ -188,6 +213,7 @@ if __name__ == '__main__':
     print('Loading & prepping data')
     N, data_type = args.N, args.data_type
     input_ids, attention_masks, labels, label_dict = load_data(N,f_metadata,data_type)
+    print('# classes = {}'.format(len(label_dict)))
         
     train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels, 
                                                                 random_state=2018, test_size=0.1)
@@ -218,7 +244,7 @@ if __name__ == '__main__':
     validation_sampler = SequentialSampler(validation_data)
     validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
     
-    print('Loading & prepping data')
+    print('Finished loading & prepping data')
     
     #Model
     print("Loading model")
@@ -238,6 +264,7 @@ if __name__ == '__main__':
     
     #Test and train
     # Store our loss and accuracy for plotting
+    val_loss_set = []
     train_loss_set = []
     epochs = args.epochs 
 
@@ -274,50 +301,79 @@ if __name__ == '__main__':
             # Update parameters and take a step using the computed gradient
             optimizer.step()
             scheduler.step()
-
+            
         tr_loss += loss.item()
         nb_tr_examples += b_input_ids.size(0)
         nb_tr_steps += 1
         print("Train loss: {}".format(tr_loss/nb_tr_steps))
-        model.eval()
-
-        #Tracking variables 
-        eval_loss, eval_accuracy = 0, 0
-        nb_eval_steps, nb_eval_examples = 0, 0
-
-      #Evaluate data for one epoch
-        acc1, acc3, acc5, logliklihood = 0,0,0,0
-        for batch in validation_dataloader:
-            # Add batch to GPU
-            batch = tuple(t.to(device) for t in batch)
-            # Unpack the inputs from our dataloader
-            b_input_ids, b_input_mask, b_labels = batch
-            # Telling the model not to compute or store gradients, saving memory and speeding up validation
-            with torch.no_grad():
-                # Forward pass, calculate logit predictions
-                outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
-                logits = outputs[0]
-
-            # Move logits and labels to CPU
-            logits = logits.detach().cpu().numpy()
-            label_ids = b_labels.to('cpu').numpy()
-
-            #evaluate
-            acc1_temp, acc3_temp, acc5_temp, logliklihood_temp = evaluate(logits,label_ids)
-
-            acc1 += acc1_temp
-            acc3 += acc3_temp
-            acc5 += acc5_temp
-            logliklihood += logliklihood_temp
-            nb_eval_steps += 1
         
-    acc1 /= nb_eval_steps
-    acc3 /= nb_eval_steps
-    acc5 /= nb_eval_steps
+        #Find val loss -- NEW CODE HERE
+        printValLoss = False
+        if printValLoss:
+            model.eval()
+            val_loss = 0
+            nb_eval_steps, nb_eval_examples = 0, 0
+
+            #Evaluate data for one epoch
+            for batch in validation_dataloader:
+                # Add batch to GPU
+                batch = tuple(t.to(device) for t in batch)
+                # Unpack the inputs from our dataloader
+                b_input_ids, b_input_mask, b_labels = batch
+                # Telling the model not to compute or store gradients, saving memory and speeding up validation
+                with torch.no_grad():
+                    # Forward pass, calculate logit predictions
+                    outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+                    print('val outputs = {}'.format(outputs))
+                    loss = outputs[0]
+                    val_loss_set.append(loss)
+            val_loss += loss.item()
+            nb_eval_examples += b_input_ids.size(0)
+            nb_eval_steps += 1
+            print("Val loss: {}".format(val_loss/nb_eval_steps))
+        
+    print('Training done: evaluating')
     
-    num_examples = len(batch)*nb_eval_steps
-    perplexity = 2** (-logliklihood / num_examples / np.log(2))
-    line = '{}: top1, top3, top5, perplexity = {:.2f}, {:.2f}, {:.2f}, {:.2f}'.format(data_type, acc1,acc3,acc5,perplexity)
+    #Tracking variables 
+    model.eval()
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+
+    #Evaluate data for one epoch
+    top1, top3, top5, logliklihood = 0,0,0,0
+    for batch in validation_dataloader:
+        # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels = batch
+        # Telling the model not to compute or store gradients, saving memory and speeding up validation
+        with torch.no_grad():
+            # Forward pass, calculate logit predictions
+            outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+            logits = outputs[0]
+
+        # Move logits and labels to CPU
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+
+        #evaluate
+        #print('logits.shape, labels = {}, {}'.format(logits.shape, label_ids))
+        #np.savetxt('logits.txt',logits)
+        #np.savetxt('labels.txt',label_ids)
+        top1_temp, top3_temp, top5_temp, logliklihood_temp = evaluate(logits,label_ids)
+
+        top1 += top1_temp
+        top3 += top3_temp
+        top5 += top5_temp
+        logliklihood += logliklihood_temp
+        nb_eval_steps += 1
+
+    #Normalize: total number = nb_evla
+    total = 1.0*(nb_eval_steps*batch_size)
+    acc1, acc3, acc5, logliklihood = top1 / total, top3 / total, top5 / total, logliklihood / total
+    perplexity = 2**(-logliklihood / total)
+    line = '{}: top1, top3, top5, perplexity (base-2) = {:.2f}, {:.2f}, {:.2f}, {:.2f}'.format(data_type, \
+                                                                                               acc1,acc3,acc5,perplexity)
     print(line)
    
     #Save data
