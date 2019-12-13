625e8b8e60ef54792c6a098b866fb2ae6bc14e09
khev <kevin.p.okeeffe@gmail.com>
2019-12-13 16:31:59 -0500

    added script for sampling from finetuned lm

--- temp.sh
+++
@@ -1,5 +0,0 @@
-input='output/language-model-title/test-docs-for-prompts.txt'
-while IFS= read -r line
-do
-  python run_generation.py --model_type=gpt2 --model_name_or_path=output/language-model-title --length=50 --prompt="$line"
-done < "$input"
